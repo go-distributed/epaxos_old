160b6f36707b282c28361488467f201e017d3b70
evan-gu <guyifan1121@gmail.com>
2014-01-10 13:38:26 -0500

    add two comments

--- command/command.go
+++ command/command.go
@@ -3,6 +3,9 @@ package command
 type Command []byte
 
 type StateMachine interface {
+	// Execute a group of commands, return the result
+	// in an array, return any error that occurs
 	Execute(c []Command) ([]interface{}, error)
+	// Test if there exists any conflicts in two group of commands
 	HaveConflicts(c1 []Command, c2 []Command) bool
 }
