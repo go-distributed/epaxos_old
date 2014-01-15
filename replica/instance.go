2135b8d8b1ed88d626481924f07619d50fd31933
Hongchao Deng <fengjingchao@gmail.com>
2014-01-15 15:51:30 -0500

    refactor ballot

--- replica/instance.go
+++ replica/instance.go
@@ -11,18 +11,6 @@ const (
 	executed
 )
 
-const (
-	// Ballot has a format like:
-	// Epoch   | Ballot  | ReplicaId
-	// 20 bits | 36 bits | 8 bits
-	ballotEpochWidth     uint64 = 20
-	ballotBallotWidth    uint64 = 36
-	ballotReplicaIdWidth uint64 = 8
-	ballotEpochMask      uint64 = ((1 << ballotEpochWidth) - 1) << (ballotBallotWidth + ballotReplicaIdWidth)
-	ballotBallotMask     uint64 = (^((1 << ballotReplicaIdWidth) - 1)) & ((1 << (ballotBallotWidth + ballotReplicaIdWidth)) - 1)
-	ballotReplicaIdMask  uint64 = (1 << ballotReplicaIdWidth) - 1
-)
-
 // a bookkeeping for infos like maxBallot, # of nack, # of ok, etc
 type InstanceInfo struct {
 	preaccCnt     int
@@ -42,23 +30,4 @@ type Instance struct {
 
 func (Inst *Instance) allReplyTheSame() bool {
 	return Inst.info.haveDiffReply == false
-}
-
-func makeLargerBallot(b uint64) uint64 {
-	remain := b & ballotEpochMask & ballotReplicaIdMask
-	ballot := b & ballotBallotMask
-	ballot = (ballot + (1 << ballotReplicaIdWidth)) & ballotBallotMask
-	return remain | ballot
-}
-
-func getEpoch(b uint64) uint64 {
-	return b & ballotEpochMask
-}
-
-func makeBallot(epoch, replicaId uint64) uint64 {
-	return (epoch << (64 - ballotEpochWidth - ballotBallotWidth)) | replicaId
-}
-
-func isInitialBallot(b uint64) bool {
-	return (b & ballotBallotMask) == 0
 }
